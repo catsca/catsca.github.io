627395bd83744c2b0969f6ed6409fc30691ad984
catsca <82754606+catsca@users.noreply.github.com>
2021-04-28 00:47:33 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.scss';
 import UserForm from './components/UserForm';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 
 function App(props) {
 
   return (
-    <div className="wdg__body">
-      <div className="wdg__container">
-        <div>
-          <UserForm />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <div className="wdg__body">
+          <div className="wdg__container">
+            <div>
+              <UserForm />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+  </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+const theme = createMuiTheme({
+  palette: {
+    primary:{main: '#009c87'},
+    secondary: {main: '#f02f2f'}
+  },
+})
+export default theme
