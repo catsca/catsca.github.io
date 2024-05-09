1f98647c9ad13929d17e88da2c9cae032df54a6a
catsca <catalin.scantee@gmail.com>
2024-05-09 23:02:22 +0300

    delete everything

--- src/theme.js
+++
@@ -1,8 +0,0 @@
-import { createMuiTheme }  from '@material-ui/core/styles'
-const theme = createMuiTheme({
-  palette: {
-    primary:{main: '#009c87'},
-    secondary: {main: '#f02f2f'}
-  },
-})
-export default theme
